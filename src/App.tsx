081f313282fe11af497540e9ebe71f81a950aba6
Amala <amala@amalas-air.lan>
2023-04-24 12:33:37 +1200

    Update basic informations title, logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      Battleship game
+      <footer>
+        <a href="https://www.flaticon.com/free-icons/ship" title="ship icons">
+          Ship icons created by Freepik - Flaticon
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
